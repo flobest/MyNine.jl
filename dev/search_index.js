8a4e0431aee0db333fc94d97bd2f865c1105841c
Documenter.jl <documenter@juliadocs.github.io>
2022-04-28 20:15:02 +0000

    build based on 9822636

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyNine","category":"page"},{"location":"#MyNine","page":"Home","title":"MyNine","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyNine.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyNine]","category":"page"}]
+}
