9822636093b9eb47910b920e83dfe8555101a788
Florian Besten <flo.best@gmx.de>
2022-04-28 22:11:35 +0200

    Add THE question

--- src/MyNine.jl
+++ src/MyNine.jl
@@ -1,5 +1,16 @@
 module MyNine
 
-# Write your package code here.
+export answere_to_all_questions
+
+function answere_to_all_questions(question::String)
+    answer::Int64 = 0
+    println(question)
+    if contains(question, "?")
+        answer = 42
+    else
+        answer = 42
+    end
+    return answer
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using MyNine
 using Test
 
 @testset "MyNine.jl" begin
-    # Write your tests here.
+    @test answere_to_all_questions("Why are we here?") == 42
 end
